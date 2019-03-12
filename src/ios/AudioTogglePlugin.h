ce3441cecc4db56f371eb536b222f7c7dcc9a8a3
Jannis Muething <js.muething@gmail.com>
2019-03-12 16:01:01 +0100

    MPVolume control: ability to control audio route

--- src/ios/AudioTogglePlugin.h
+++ src/ios/AudioTogglePlugin.h
@@ -1,5 +1,8 @@
 #import <Cordova/CDVPlugin.h>
 
+FOUNDATION_EXPORT NSString *const SpeakerSelectedNotification;
+FOUNDATION_EXPORT NSString *const AudioOptionsAvailable;
+
 @interface AudioTogglePlugin : CDVPlugin
 - (void)setAudioMode:(CDVInvokedUrlCommand*)command;
 @end
